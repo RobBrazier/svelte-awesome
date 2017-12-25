36135a3765120a28c81927c45a7f9f68903450e1
Rob Brazier <RobBrazier@users.noreply.github.com>
2017-12-25 13:27:14 +0000

    build: fix semantic-release condition verification

--- build/verify-conditions.js
+++ build/verify-conditions.js
@@ -1,7 +1,7 @@
 const SRError = require('@semantic-release/error');
 
 module.exports = function (pluginConfig, config, callback) {
-  if (config.env.SEMAPHORE === 'true') {
+  if (process.env.SEMAPHORE === 'true') {
     callback(null);
   } else {
     callback(new SRError('Not running on Semaphore, won\'t be published.'));
