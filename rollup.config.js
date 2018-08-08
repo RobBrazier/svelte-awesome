598199971896f99e32046b3a253fca56b1aa3fe8
Rob Brazier <RobBrazier@users.noreply.github.com>
2018-08-08 19:27:43 +0100

    feat: add svelte property to package.json
    fixes #244
    * feat: add svelte property to package.json
    * build(deps-dev): bump rollup from 0.63.4 to 0.64.1
    * build(devdeps): bump rollup-plugin-uglify from 3.0.0 to 4.0.0
    * feat: update svelte property as src is in .npmignore

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
 const pkg = require('./package.json');
 
+const name = pkg.name
+  .replace(/^\w/, m => m.toUpperCase())
+  .replace(/-\w/g, m => m[1].toUpperCase());
+
 module.exports = {
   input: './src/index.js',
   output: {
     file: pkg.main,
     format: 'umd',
-    name: 'SvelteAwesome'
+    name
   },
   context: 'window',
   plugins: [
@@ -16,6 +20,6 @@ module.exports = {
     require('rollup-plugin-replace')({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
-    require('rollup-plugin-uglify')()
+    require('rollup-plugin-uglify').uglify()
   ]
 };
