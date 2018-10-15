e1a94f93b64bab67ef28707d6023f5063578e720
Antony Jones <aj@desirableobjects.co.uk>
2018-10-15 17:01:19 +0100

    fix(ssr): make SSR compatible

--- src/icons/play.js
+++ src/icons/play.js
@@ -1,3 +1,3 @@
-import Icon from '../components/Icon.html';
 
-Icon.register({ play: { width: 1408, height: 1792, paths: [{ d: 'M1384 927l-1328 738q-23 13-39.5 3t-16.5-36v-1472q0-26 16.5-36t39.5 3l1328 738q23 13 23 31t-23 31z' }] } });
+
+export default { play: { width: 1408, height: 1792, paths: [{ d: 'M1384 927l-1328 738q-23 13-39.5 3t-16.5-36v-1472q0-26 16.5-36t39.5 3l1328 738q23 13 23 31t-23 31z' }] } };
