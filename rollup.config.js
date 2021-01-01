11aab65dc707cd18bf1753c3a258cb9b3eea005f
Rob Brazier <RobBrazier@users.noreply.github.com>
2021-01-01 13:07:19 +0000

    chore: add css plugin to main rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
+import css from 'rollup-plugin-css-only';
 import pkg from './package.json';
 
 const name = pkg.name
@@ -15,6 +16,7 @@ export default {
 	],
 	plugins: [
 		svelte(),
+		css(),
 		resolve()
 	]
 };
