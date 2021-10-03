8d3ed0fa91f9dcafa8c6f65effb580d10cbb43c4
Rob Brazier <RobBrazier@users.noreply.github.com>
2021-10-03 17:47:50 +0100

    chore: standardise svelte input

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,2 @@
-import Icon from './components/Icon.svelte';
-
-import './icons';
-
-export default Icon;
+export { default as default} from './Icon.svelte';
+export { default as Icon } from './Icon.svelte';
