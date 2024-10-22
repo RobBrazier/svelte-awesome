776b0fbc79d6c51147ca76e8efcbe64009d81b5c
Rob Brazier <git+github@brzr.co>
2024-10-22 13:24:39 +0100

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      });
+    in
+    {
+      overlays.default = final: prev: rec {
+        nodejs = prev.nodejs_20;
+      };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ node2nix nodejs nodePackages.pnpm ];
+        };
+      });
+    };
+}
